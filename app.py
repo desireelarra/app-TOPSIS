448f40fa8de66d3f98553d35e9231ecd72db0371
desireelarra <desireelarrazabal@gmail.com>
2026-04-21 21:56:35 -0600

    Fix indentation and update manual input message

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ metodo_entrada = st.radio(
 df_editado = None
 
 # --- OPCIÓN 1: ENTRADA MANUAL ---
-st.write("Configura el tamaño de tu tabla dadas tus opciones y criterios, para después llenar los datos directamente en la tabla de abajo.")
 if metodo_entrada == "Agregar manualmente los datos de mi tabla (menor a 6 filas o columnas)": #Poner la matriz de manera manual.
+    st.write("Configura el tamaño de tu tabla dadas tus opciones y criterios, para después llenar los datos directamente en la tabla de abajo.")
     col1, col2 = st.columns(2)
     with col1:
         num_alternativas = st.number_input("Número de Opciones (Filas)", min_value=2, max_value=20, value=4)
